7e9a93e8fa1131b3538897d99fab2d70d1547db5
Ashish Shaji <43558686+ashishshaji@users.noreply.github.com>
2020-12-31 12:25:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/result')
-def res():
-    return render_template("result.html")
+
 
 @app.route('/city',methods=['POST','GET'])
 def search_city():
@@ -40,30 +38,6 @@ def search_city():
 
 
 
-"""@app.route('/city',methods=['POST','GET'])
-def search_city():
-    if request.method == 'POST':
-        API_key = 'b20079dcfeb1f6d220aa1d3733d8692c'  # initialize your key here
-        city = request.args.get('q')  # city name passed as argument
-
-        # call API and convert response into Python dictionary
-        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}'
-        response = requests.get(url).json()
-
-        # error like unknown city name, inavalid api key
-        if response.get('cod') != 200:
-            message = response.get('message', '')
-            return f'Error getting temperature for {city.title()}. Error message = {message}'
-
-        # get current temperature and convert it into Celsius
-        current_temperature = response.get('main', {}).get('temp')
-        if current_temperature:
-            current_temperature_celsius = round(current_temperature - 273.15, 2)
-            return f'Current temperature of {city.title()} is {current_temperature_celsius} &#8451;'
-        else:
-            return f'Error getting temperature for {city.title()}'
-
-"""
 
 
 
