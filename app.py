211690e16f99cdfb864560a4d4fa91e4329ebea3
ashishshaji <ashishshaji3344@gmail.com>
2020-12-31 12:04:36 +0530

    completed

--- app.py
+++ app.py
@@ -19,11 +19,16 @@ def search_city():
         url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=b20079dcfeb1f6d220aa1d3733d8692c'
         response = requests.get(url).json()
         current_temperature = response.get('main', {}).get('temp')
+        description = response['weather'][0]['description']
+        icon = response['weather'][0]['icon']
+
         if current_temperature:
             current_temperature_celsius = round(current_temperature - 273.15, 2)
             weather={
                 'temp_cel':current_temperature_celsius,
-                'city_name':city_name
+                'city_name':city_name,
+                'description': description,
+                'icon': icon
 
             }
             return render_template("result.html",weather=weather )
